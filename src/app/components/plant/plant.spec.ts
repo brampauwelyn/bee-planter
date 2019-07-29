585f60f202e5539b720e4ee50dea70fd57954965
brampauwelyn <brampauw@gmail.com>
2019-07-29 22:52:43 +0200

    added test file for plant component

---
+++ src/app/components/plant/plant.spec.ts
@@ -0,0 +1,11 @@
+import { PlantComponent } from './plant.component';
+
+describe('Plantcomponent', () => {
+
+  it('should instantiate', () => {
+    const component: PlantComponent = new PlantComponent();
+    expect(component).toBeDefined();
+  });
+
+
+});
