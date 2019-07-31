b43a468af49562d99fba7c685c6e65176ddf4a5a
brampauwelyn <brampauw@gmail.com>
2019-07-31 22:19:07 +0200

    progress + add new plants

--- src/app/components/plant/plant.component.ts
+++ src/app/components/plant/plant.component.ts
@@ -10,16 +10,20 @@ import plantsData from './plants.json';
 export class PlantComponent {
   title = 'BeePlanter';
   plants: Array<any> = plantsData;
+  activeFilters = {
+    colors: [],
+  };
   filteredPlants: Array<any> = [...plantsData];
 
-  resetFilters(){
+  resetFilters() {
     this.filteredPlants = this.plants;
     return this.filteredPlants;
   }
 
   filterByColor(color: string) {
+    this.activeFilters.colors.push(color);
     this.filteredPlants = this.filteredPlants.filter( plant => {
-      return plant.color === color;
+      return this.activeFilters.colors.includes(plant.color);
     });
   }
 
@@ -27,6 +31,13 @@ export class PlantComponent {
     this.filteredPlants = this.filteredPlants.filter( plant => {
       return plant.nectar >= minValue && plant.nectar <= maxValue;
     });
+  }
+
+  setBackgroundColor(color: string){
+    const styles = {
+      'background-color': color
+    };
+    return styles;
   }
 
 }
